75bf8f0e7a51fc0a5038a2fd29beb8ccbbad8739
Adam-Belkadi <adambelkadi1@gmail.com>
2024-03-11 11:37:38 +0100

    adam2 added

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         MyFrame frame = new MyFrame();
-        System.out.println("hoho");
+        System.out.println("hohoadam");
 
     }
 
